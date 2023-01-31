a0d6615531fff581b4b7e43280bc599465ab340a
yh <2827902614@qq.com>
2023-01-31 12:11:12 +0800

    dev 01

--- src/Atmmain.java
+++ src/Atmmain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Atmmain {
     //入口类
     public static void main(String[] args) {
-        //add yy
+        //add dev01
         Scanner input = new Scanner(System.in);
         ArrayList<Account> accounts = new ArrayList<>();
         while(true) {
